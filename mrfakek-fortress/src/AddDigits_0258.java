0fdd235ef8defef9d55ce4a6c720e967d5711bd3
nick_tsygankov <kolanberat@gmail.com>
2026-09-07 21:06:58 +0300

    Add Digits 0258 solved.

---
+++ mrfakek-fortress/src/AddDigits_0258.java
@@ -0,0 +1,13 @@
+public class AddDigits_0258 {
+    public int addDigits(int num) {
+        int sum = 0;
+        while(num != 0){
+            sum = sum+num%10;
+            num=num/10;
+        }
+        if(sum > 9){
+            return addDigits(sum);
+        }
+        else return sum;
+    }
+}
